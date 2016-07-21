3adb564db45c342dffb9c2248b3c73d698d7c6cd
Den Williams <work@denwilliams.net>
2016-07-21 20:02:04 +1000

    tests for event emitter

--- src/core/shared-event-emitter.test.ts
+++ src/core/shared-event-emitter.test.ts
@@ -8,4 +8,66 @@ test.beforeEach(t => {
   t.context.eventBus = kernel.get('IEventBus');
 });
 
-test.skip('nothing yet', () => {});
+test('on receives emitted event with name and data', t => {
+  // ARRANGE
+  t.plan(2);
+  const eventBus: Homenet.IEventBus = t.context.eventBus;
+  eventBus.on('test', 'something', assertReceived);
+
+  // ACT
+  eventBus.emit('test', 'something', { a: 1 })
+
+  // ASSERT
+  function assertReceived(e) {
+    t.is(e.name, 'test.something');
+    t.deepEqual(e.data, { a: 1 })
+  }
+});
+
+test('on can use wildcard for event', t => {
+  // ARRANGE
+  t.plan(2);
+  const eventBus: Homenet.IEventBus = t.context.eventBus;
+  eventBus.on('test', '*', assertReceived);
+
+  // ACT
+  eventBus.emit('test', 'something', { a: 1 })
+
+  // ASSERT
+  function assertReceived(e) {
+    t.is(e.name, 'test.something');
+    t.deepEqual(e.data, { a: 1 })
+  }
+});
+
+test('on can use wildcard for source', t => {
+  // ARRANGE
+  t.plan(2);
+  const eventBus: Homenet.IEventBus = t.context.eventBus;
+  eventBus.on('*', 'something', assertReceived);
+
+  // ACT
+  eventBus.emit('test', 'something', { a: 1 })
+
+  // ASSERT
+  function assertReceived(e) {
+    t.is(e.name, 'test.something');
+    t.deepEqual(e.data, { a: 1 })
+  }
+});
+
+test('on can use wildcard within event', t => {
+  // ARRANGE
+  t.plan(2);
+  const eventBus: Homenet.IEventBus = t.context.eventBus;
+  eventBus.on('*', 'something.*', assertReceived);
+
+  // ACT
+  eventBus.emit('test', 'something.blah', { a: 1 })
+
+  // ASSERT
+  function assertReceived(e) {
+    t.is(e.name, 'test.something.blah');
+    t.deepEqual(e.data, { a: 1 })
+  }
+});
