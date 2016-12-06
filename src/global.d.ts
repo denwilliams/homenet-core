ef71baab5690499867acce165784a288bb4c720a
Den Williams <work@denwilliams.net>
2016-12-06 20:54:09 +1100

    add types, remove typings

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="../node_modules/node-red-contrib-scenes/node-red-contrib-scenes.d.ts" />
 /// <reference path="../node_modules/inversify-dts/inversify/inversify.d.ts" />
-/// <reference path="../typings/index.d.ts" />
 
 export { injectable } from 'inversify';
