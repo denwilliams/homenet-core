2be16fcdd7ca78926f0016500604e2f6fc38cf60
Den Williams <work@denwilliams.net>
2017-06-17 16:53:05 +1000

    oops

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -4,9 +4,9 @@ npm install
 npm run typedoc
 cd docs
 
-# git config user.name "Travis CI"
-# git config user.email "$COMMIT_AUTHOR_EMAIL"
-# git remote add gh-token "https://${GH_TOKEN}@github.com/denwilliams/homenet-core.git";
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+git remote add gh-token "https://${GH_TOKEN}@github.com/denwilliams/homenet-core.git";
 git fetch gh-token && git fetch gh-token gh-pages:gh-pages;
 
 pip install --user mkdocs
