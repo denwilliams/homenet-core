9950ab1adce0b278bbcb7efc14b798296ef36fca
Den Williams <work@denwilliams.net>
2017-09-05 18:45:43 +1000

    dont set full ID

--- src/utils/settable-class-type-manager.ts
+++ src/utils/settable-class-type-manager.ts
@@ -8,9 +8,8 @@ export abstract class SettableClassTypeManager<T> extends ClassTypeManager<T> {
   addSettableType(typeId: string, factory: Homenet.IClassTypeFactory<Homenet.ISettable>) {
     this.logger.info(`Adding ${chalk.blue(this.classId)}  type ${chalk.cyan(typeId)}`);
     this.types[typeId] = (id : string, opts : any) : T => {
-      const fullId = `${this.classId}.${id}`;
       const settable: Homenet.ISettable = factory(id, opts);
-      return this.mapSettable(fullId, settable, opts);
+      return this.mapSettable(id, settable, opts);
     };
   }
 
